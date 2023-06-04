98efe0daa83c055245760a9edc3a577548461068
davidecalza <davide.calza@tuta.io>
2023-06-04 17:54:41 +0200

    small fixes

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker compose up --build -d
 ./stream_start.sh
-docker exec -it pipeline-jobmanager flink run -py pipeline/main.py &
+docker exec -it pipeline-jobmanager flink run -py pipeline/main.py -d
