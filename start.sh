e2f35f72394e52d432fdfc24980a866b5a7b1bc8
davidecalza <davide.calza@tuta.io>
2023-06-05 17:29:59 +0200

    add sleeping time for cassandra waiting

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker compose up --build -d
+echo "Waiting 30s for Cassandra init..."
+sleep 30
 ./stream_start.sh
 docker exec -it pipeline-jobmanager flink run -py pipeline/main.py -d
