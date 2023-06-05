2b6127b67bcf8ab707b9e79937b25db8d02d778e
denisgurabardhi <128040691+denisgurabardhi@users.noreply.github.com>
2023-06-05 12:14:48 +0200

    Update __init__.py

--- app/infrastructure/__init__.py
+++ app/infrastructure/__init__.py
@@ -1,3 +1,14 @@
+'''
+The __all__ list specifies the names of the modules that are intended to be imported when using the wildcard import statement (from module import *).
+
+This code snippet imports specific modules and classes from the package:
+
+ConsumerPrint, Producer, ConsumerFlink, and ProducerFlink are imported from the broker module.
+Cache is imported from the cache module.
+Database and DatabaseTables are imported from the database module.
+These imported modules and classes can be accessed using the corresponding names in the package.
+'''
+
 __all__ = ['broker', 'cache', 'database']
 
 from .broker import ConsumerPrint, Producer, ConsumerFlink, ProducerFlink
