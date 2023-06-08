cb4519f27da77d0a5c06efdb876db058352e4520
denisgurabardhi <128040691+denisgurabardhi@users.noreply.github.com>
2023-06-08 09:02:54 +0200

    Update bank.py

--- app/model/bank.py
+++ app/model/bank.py
@@ -7,7 +7,7 @@ from app.infrastructure import DatabaseTables
 @dataclass
 class Bank:
     '''
-    Represents an account with associated attributes.
+    Represents a bank with associated attributes.
     '''
     name: str
     address: str
@@ -15,7 +15,7 @@ class Bank:
 
     def to_dict(self):
         '''
-        Convert an account object to a dictionary
+        Convert a bank object to a dictionary
         '''
         return {
             'name': self.name,
@@ -25,13 +25,13 @@ class Bank:
 
     def to_json(self):
         '''
-        Converts the Account object to a JSON string.
+        Converts the bank object to a JSON string.
         '''
         return json.dumps(self.to_dict())
 
     def submit(self, broker, topic):
         '''
-        Submits the account object to the specified broker.
+        Submits the bank object to the specified broker.
 
         :param broker: The broker to submit the data to
         :type broker:
