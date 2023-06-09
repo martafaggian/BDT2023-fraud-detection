a3b3f8f1f1a04e8ee1fee870a52b92cc2395d7d9
denisgurabardhi <128040691+denisgurabardhi@users.noreply.github.com>
2023-06-09 12:30:59 +0200

    Update fraud_detection.py

--- app/pipeline/fraud_detection.py
+++ app/pipeline/fraud_detection.py
@@ -1,7 +1,16 @@
 '''
-The provided code implements a class called "FraudDetection" that extends the "MapFunction" class
-from the pyflink.datastream.functions module. This class is designed to perform fraud detection
-on a data stream.
+The provided code implements a fraud detection logic using the Flink DataStream API. It defines a 
+FraudDetection class that extends the MapFunction class. The map method of this class applies the 
+fraud detection algorithm to each element in the input data stream, updating the element with 
+fraud-related information. The compute_fraud method computes the fraud status and confidence level 
+for a given record. If the record's "amount" field is greater than 5000, it is classified as fraud 
+with a confidence level of 1.0; otherwise, it is considered non-fraud with a confidence level of 0.0. 
+This code serves as a basic example of fraud detection and can be customized or extended with more 
+advanced techniques as needed.
+
+The code can be used as follows:
+
+stream.map(FraudDetection())
 
 '''
 
