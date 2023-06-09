f66ff4858f62387d420f99c4de6217c7727a6cba
Marta Faggian <115492086+martafaggian@users.noreply.github.com>
2023-06-09 15:20:46 +0200

    Added documentation app1.py

--- WebApp/app1.py
+++ WebApp/app1.py
@@ -7,33 +7,65 @@ The layout includes a navigation bar, input fields for email and password, a sub
 The code also defines a callback function to validate the email input.
 
 Firstly, we import the necessary modules and libraries:
-- Flask: A web framework for creating server-side applications.
-- Dash: A framework for building interactive web applications.
-- dash_bootstrap_components: A library providing Bootstrap-themed Dash components.
-- pandas: A library for data manipulation and analysis.
+-          flask: A web framework for creating server-side applications. With redirect() a response object (namely, a WSGI application) is returned and 
+                  it redirects the client to the target location
+-          dash: A framework for building interactive web applications. Some additional components are used:
+                -     Dash HTML Components (html) is used to access HTML tags
+                -     Input and  Output that hold properties for specificied components
+        
+-          dash_bootstrap_components: A library providing Bootstrap-themed Dash components.
+-          pandas: A library for data manipulation and analysis.
 
+These dependencies can be installed by using the following command:
+
+             pip install flask dash dash_bootstrap_components pandas
 """
 
 from flask import Flask, redirect
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, html, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
+
+""""
+# === INITIALIZATION === #
+The Flask and Dash applications are initialized using the following steps:
+-          An instance of the Flask application (app_flask1) is created.
+-          The external stylesheets are defined using dash_bootstrap_components's SOLAR theme.
+-          An instance of the Dash application (app1) is created, with the Flask application as the server.
+-          The URL base pathname is set to / for the Dash application. 
+""""
 
 app_flask1 = Flask(__name__)
 external_stylesheets = [dbc.themes.SOLAR]
 app1 = Dash(__name__, server=app_flask1, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True,
            url_base_pathname='/')
-
+"""
+The code reads transaction data from a CSV file named transactions.csv using the pandas library. 
+It extracts the unique user IDs from the data.
+"""
 data = pd.read_csv('transactions.csv')
 users = data['user_id'].unique()
 
+"""
+# === APPLICATION LAYOUT === #
+
+The layout of the web application is defined using HTML and Dash components. It includes the following elements:
+-	A container for the application content.
+-	A row containing a title and a description.
+-	A navigation bar with links to GitHub, settings, and FAQ pages.
+-	Input fields for email and password.
+-	A submit button for form submission.
+-	A link to the selection page.
+-	An alert message for providing additional information.
+-	An image related to fraud detection (for aesthetic purposes)
+"""
 app1.layout = html.Div([
     dbc.Container([
         dbc.Row([
-
-            html.H1(["Welcome to DetectiveFraud"]),
-            html.H2(["Keep your transactions safe from thieves!"
-        ]),html.P('', style={'margin-bottom': '20px'}),]),
+                   html.H1(["Welcome to DetectiveFraud"]),
+                   html.H2(["Keep your transactions safe from thieves!"
+                ]),
+                   html.P('', style={'margin-bottom': '20px'}),]),
         dbc.NavbarSimple(
             children=[
                 dbc.NavItem(dbc.NavLink("GitHub", href="https://github.com/martafaggian/BDT2023-fraud-detection")),
@@ -46,13 +78,13 @@ app1.layout = html.Div([
                     nav=True,
                     in_navbar=True,
                     label="More",
-                ),
+                      ),
             ],
             brand="Start your Research!",
             brand_href="#",
             color="primary",
             dark=True,
-        ),html.P('', style={'margin-bottom': '20px'}),
+                      ),html.P('', style={'margin-bottom': '20px'}),
     dbc.Row(
                 [
                     dbc.Label("Email", width="auto"),
@@ -77,8 +109,8 @@ app1.layout = html.Div([
                 html.Div([
                     dbc.Button("Go to Selection", color="primary", className="me-1", href='http://localhost:2070/')
                 ])
-            ])
-,html.P('', style={'margin-bottom': '20px'}),
+            ]),
+               html.P('', style={'margin-bottom': '20px'}),
         dbc.Row([
             dbc.Alert(
                 [
@@ -91,11 +123,15 @@ app1.layout = html.Div([
         dbc.Row([
                     html.Div([
                     html.Img(src="https://cdn.pixabay.com/photo/2022/03/13/01/36/fraud-7065116_1280.png")
+        ])])
 ])])
 
-        ])])
-
-
+"""
+# === CALLBACK FUNCTION AND FLASK ROUTE === #
+The code then defines a callback function using the app1.callback decorator. 
+The callback function is triggered by changes in the email input field. 
+It checks if the entered email ends with "@gmail.com" and updates the validity state of the email input field accordingly.
+""""
 @app1.callback(
     [Output("email-input", "valid"), Output("email-input", "invalid")],
     [Input("email-input", "value")],
@@ -105,12 +141,20 @@ def check_validity(text):
         is_gmail = text.endswith("@gmail.com")
         return is_gmail, not is_gmail
     return False, False
-
+"""
+The code defines a Flask route for the homepage ("/") using the @app_flask1.route decorator. 
+The route redirects to the root URL ("/").
+"""
 
 @app_flask1.route('/')
 def homepage():
     return redirect('/')
 
+"""
+ # === EXECUTION === #
+The code checks if the current module is being run as the main script by using the __name__ variable.
+If it is the main script, the Flask application (app_flask1) is run in debug mode on port 5050 (it can be ommitted, but for persistency purposes, it is specified).
+"""
 
 if __name__ == "__main__":
     app_flask1.run(debug=True, port= 5050)
